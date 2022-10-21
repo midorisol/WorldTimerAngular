b646533fce6d6dee5ef68757e07cf1bb6b751784
Mark Csoma <84617390+midorisol@users.noreply.github.com>
2022-10-21 20:31:21 +0200

    newhomework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   selectedTimeZone = 'Europe/Budapest';
 
+  timezonesList = [
+    'Europe/Budapest',
+    'America/New_York',
+    'Atlantic/Reykjavik',
+    'Australia/Sydney',
+    'Iceland',
+    'Japan'
+  ];
+
+  displayedTimezones = [
+    'Europe/Budapest',
+    'America/New_York',
+  ];
+
   updateSelectedTimeZone(timeZone: string) {
     this.selectedTimeZone = timeZone;
+  }
+
+  addNewTimezone(){
+    this.displayedTimezones.push(this.timezonesList[this.displayedTimezones.length]);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { WorldtimerComponent } from './worldtimer/worldtimer.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { WorldtimerComponent } from './worldtimer/worldtimer.component';
     WorldtimerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
